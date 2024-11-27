aa30d87deaf1897db9157496840323bf3bd60bdc
wss0702 <112996085+wss0702@users.noreply.github.com>
2024-11-27 17:10:36 +0900

    app.jsx수정

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Header from './components/layout/Header/Header';
 import WishlistPage from './components/pages/wishlist/Wishlist';
 import PopularPage from './components/pages/popular/Popular';
 import HomeMain from './components/pages/main/HomeMain';
-import Login from './components/pages/signin/login';
+import Login from './components/pages/signin/Login';
 import SearchPage from './components/pages/search/Search';
 import { ToastContainer } from 'react-toastify';
 
